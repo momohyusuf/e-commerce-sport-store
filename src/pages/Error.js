8bffb579e63e07f48bd41ab2c99f4fa14e5e134e
Momoh Yusuf <ymomoh6@outlook.com>
2022-06-14 18:14:47 +0100

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Error() {
-  return <p> sorry the page your looking for does not exist </p>;
+  return <p> sorry the page your're looking for does not exist </p>;
 }
 
 export default Error;
